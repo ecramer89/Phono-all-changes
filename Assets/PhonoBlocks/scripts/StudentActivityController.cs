0dd222085f075a879fc3820d4f4309a2e302bbfc
Emily Cramer <emily.cramer@adaptechsolutions.ca>
2017-09-05 11:51:22 -0700

    provided the SAC with reference to submit word button. SAC de activates button in remove all letters mode and re activates when next problem begins. added comments to state and other fields of SAC

--- Assets/PhonoBlocks/scripts/StudentActivityController.cs
+++ Assets/PhonoBlocks/scripts/StudentActivityController.cs
@@ -10,9 +10,9 @@ public class StudentActivityController : MonoBehaviour
 
 		enum State
 		{
-				MAIN_ACTIVITY,
-		        FORCE_CORRECT_LETTER_PLACEMENT,
-		        REMOVE_ALL_LETTERS
+				MAIN_ACTIVITY, //standard functionality (accept user input; update UI; update colors/error feedback)
+		        FORCE_CORRECT_LETTER_PLACEMENT, //don't update the GUI letter images unless the inputted letter is correct. still show error feedback.
+		        REMOVE_ALL_LETTERS //after the problem is completed & before going to next problem, force user to remove all the tangible letters they've placed on the platform
 			
 		}
 
@@ -62,6 +62,9 @@ public class StudentActivityController : MonoBehaviour
 		AudioClip removeAllLetters;
 		AudioClip triumphantSoundForSessionDone;
 
+		//todo refactor; should subscribe to appropriate events. SAC should publish appropriate events.
+		GameObject submitWordButton;
+
 	   
 		public string UserChangesAsString {
 				get {
@@ -87,8 +90,7 @@ public class StudentActivityController : MonoBehaviour
 			
 				hintController = gameObject.GetComponent<HintController> ();
 				hintController.Initialize (hintButton);
-			
-				SetUpNextProblem ();
+
 	
 			    //cache audio clips
 				excellent = InstructionsAudio.instance.excellent;
@@ -100,6 +102,12 @@ public class StudentActivityController : MonoBehaviour
 				removeAllLetters = InstructionsAudio.instance.removeAllLetters;
 
 				triumphantSoundForSessionDone = InstructionsAudio.instance.allDoneSession;
+
+		       //todo: refactor so that events are sent and the button disables itself.
+				submitWordButton = GameObject.Find("CheckWordButton");
+
+
+				SetUpNextProblem ();
 		}
 
 		
@@ -115,6 +123,7 @@ public class StudentActivityController : MonoBehaviour
 				StudentsDataHandler.instance.RecordActivityTargetWord (currProblem.TargetWord (false));
 
 		       //decode the target word; cache the decoded letter sound components.
+		       //this allows us to easily access the expected multi letter unit or color of target letter for each given position.
 		        targetWordAsLetterSoundComponents = LetterSoundComponentFactoryManager.Decode (currProblem.TargetWord (true), 
 		                                                                               SessionsDirector.IsSyllableDivisionActivity);
 
@@ -129,12 +138,11 @@ public class StudentActivityController : MonoBehaviour
 		        //turn on the underlines that indicate which letters the child has to place
 				arduinoLetterController.activateLinesBeneathLettersOfWord(currProblem.TargetWord(true));
 		        
-				UserInputRouter.instance.RequestTurnOffImage ();
-				hintController.DeActivateHintButton ();
-			
+
 				PlayInstructions (); //dont bother telling to place initial letters during assessment mode
 
 				state = State.MAIN_ACTIVITY;
+				submitWordButton.SetActive (true);
 
 		        
 
@@ -278,9 +286,15 @@ public class StudentActivityController : MonoBehaviour
 					}
 						break;
 				case State.REMOVE_ALL_LETTERS:
-					if (letter != ' ')
+					if (letter != ' ') //don't bother updating the letter unless the user removed it
 						return;
-						arduinoLetterController.ChangeTheLetterOfASingleCell (atPosition, letter);
+			
+					arduinoLetterController.ChangeTheLetterOfASingleCell (atPosition, letter);
+					//once the user removes all letters from the current problem; automatically turn off the display image and go to the next activity.
+					if(allUserControlledLettersAreBlank()){ 
+						UserInputRouter.instance.RequestTurnOffImage ();
+						HandleEndOfActivity ();
+					}
 						break;
 					}
 		}
@@ -305,13 +319,7 @@ public class StudentActivityController : MonoBehaviour
 
 		public void HandleSubmittedAnswer ()
 		{     
-			if (state == State.REMOVE_ALL_LETTERS) {
-			   //go to next problem if all the letters were removed.
-				if(allUserControlledLettersAreBlank()) {
-					HandleEndOfActivity ();
-				}
-				
-			} else {
+		        
 				StudentsDataHandler.instance.LogEvent ("submitted_answer", UserChangesAsString, currProblem.TargetWord (false));
 					
 				currProblem.IncrementTimesAttempted ();
@@ -330,8 +338,7 @@ public class StudentActivityController : MonoBehaviour
 					HandleIncorrectAnswer ();				
 					
 				}
-			}
-				
+
 
 		}
 
@@ -372,7 +379,11 @@ public class StudentActivityController : MonoBehaviour
 				StudentsDataHandler.instance.SaveActivityDataAndClearForNext (currProblem.TargetWord (false), currProblem.InitialWord);
 		        
 		     
+		        //require user to remove all of the tangible letters from the platform before advancing to the next problem.
+		        //don't want the letters still on platform from problem n being interpreted as input for problem n+1.
 				state = State.REMOVE_ALL_LETTERS;
+				//disable submit button; we automatically go to next problem when user removes all letters. 
+				submitWordButton.SetActive (false);
       
 		}
 
