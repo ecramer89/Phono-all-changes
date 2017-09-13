5c090ab1366a1156c584908092d29c6b8bb81efa
Emily Cramer <emily.cramer@adaptechsolutions.ca>
2017-09-13 11:55:23 -0700

    modified session button to conform to similar design pattern as other buttons

---
+++ Assets/PhonoBlocks/scripts/buttons/SessionButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(UIButtonMessage))]
+public class SessionButton : MonoBehaviour {
+	[SerializeField] int session;
+
+	void Start(){
+		Events.Dispatcher.OnModeSelected += (Mode mode) => {
+			if (mode == Mode.STUDENT) {
+				UIButtonMessage messenger = GetComponent<UIButtonMessage> ();
+				messenger.target = gameObject;
+				messenger.functionName = "SelectSession";
+				messenger.trigger = UIButtonMessage.Trigger.OnClick;
+				gameObject.SetActive(true);
+			} else {
+				gameObject.SetActive(false);
+			}
+		};
+	}
+
+
+
+	void SelectSession(){
+
+		Events.Dispatcher.RecordSessionSelected (session);
+		gameObject.SetActive(false);
+
+	}
+}
