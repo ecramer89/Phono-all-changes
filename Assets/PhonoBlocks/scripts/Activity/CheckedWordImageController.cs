e46e660f9cbba662f02c568ec11459aadab17e57
Emily Cramer <emily.cramer@adaptechsolutions.ca>
2017-10-30 15:29:22 -0700

    checked word image offsets in bothn modes when last letter is removed

--- Assets/PhonoBlocks/scripts/Activity/CheckedWordImageController.cs
+++ Assets/PhonoBlocks/scripts/Activity/CheckedWordImageController.cs
@@ -7,12 +7,6 @@ public class CheckedWordImageController : PhonoBlocksSubscriber
 {
 		GameObject checkedWordImage;
 		UITexture img;
-		BoxCollider clickTrigger;
-		long showTime = -1;
-		bool disableTextureOnPress;
-		long defaultDisplayTime = 2000;
-		bool caller_ends_display;
-
 
 	public override void SubscribeToAll(PhonoBlocksScene nextToLoad){
 		if(nextToLoad == PhonoBlocksScene.MainMenu) return;
@@ -21,13 +15,7 @@ public class CheckedWordImageController : PhonoBlocksSubscriber
 				checkedWordImage = GameObject.Find("CheckedWordImage");
 				img = checkedWordImage.GetComponent<UITexture> ();
 				img.enabled = false;
-				clickTrigger = checkedWordImage.GetComponent<BoxCollider> ();
-				clickTrigger.enabled = false;
 			});
-		//remove the old image, if it's still there
-		Transaction.Instance.NewProblemBegun.Subscribe(this,(ProblemData problem) => {
-			EndDisplay ();
-		});
 		//display the target word image
 		Transaction.Instance.CurrentProblemCompleted.Subscribe(this,DisplayTargetWord);
 		//level three hint; show the image of the target word.
@@ -40,12 +28,19 @@ public class CheckedWordImageController : PhonoBlocksSubscriber
 		//if the current state of user input letters corresponds to a saved image, then
 		//display it.
 		Transaction.Instance.UserAddedWordToHistory.Subscribe(this,DisplayCurrentInputWord);
+
+			/*
+			 * end the display once all letters have been removed
+			 * */
+			Transaction.Instance.UserEnteredNewLetter.Subscribe(this, (char newLetter,int position)=>{
+					if(newLetter == ' ' && Transaction.Instance.State.UserInputLetters.Trim()==""){
+						EndDisplay();
+					}
+			});
 		}
 
 	}
-
-
-
+		
 		void DisplayCurrentInputWord(){
 			DisplayImageForWordIfAny (Transaction.Instance.State.UserInputLetters);
 		}
@@ -59,84 +54,25 @@ public class CheckedWordImageController : PhonoBlocksSubscriber
 			word = word.Trim ();
 			Texture2D newimg = (Texture2D)Resources.Load ($"{Parameters.FILEPATHS.RESOURCES_WORD_IMAGE_PATH}{word}", typeof(Texture2D));
 			if (!ReferenceEquals (newimg, null)) {
-				ShowImage (newimg, false, true);
+				ShowImage (newimg);
 			}
 
 		}
 
-		public void ShowImage (Texture2D newimg, long showTime)
-		{
-				this.showTime = showTime;
-				SetAndEnableTexture (newimg);
 
-
+		void ShowImage (Texture2D newImg)
+		{   				
+			img.mainTexture = newImg;
+			img.enabled = true;
 		}
 
-		public void ShowImage (Texture2D newimg, bool disableTextureOnPress, bool caller_ends_display=false)
-		{
-				this.caller_ends_display = caller_ends_display;
-				if (newimg != null) {
-						if (disableTextureOnPress) {
-								this.disableTextureOnPress = disableTextureOnPress;
-								SetAndEnableTexture (newimg);
-								clickTrigger.enabled = true;
-						} else
-								ShowImage (newimg, defaultDisplayTime);
-				}
-		}
 
-		void SetAndEnableTexture (Texture2D newImg)
+		void EndDisplay ()
 		{
-				img.mainTexture = newImg;
-				img.enabled = true;
-		}
-
-		void OnPress (bool isPressed)
-		{
-				if (isPressed && disableTextureOnPress) {
-						EndDisplay ();
-				}
-		}
-
-		public void EndDisplay ()
-		{
-
-		    
 				img.enabled = false;
-				caller_ends_display = false;
-				if (disableTextureOnPress) {
-						disableTextureOnPress = false;
-						clickTrigger.enabled = false;
-				}
-				if (showTime > 0) {
-						showTime = -1;
-				}
+			
 
 		}
-
-		void Update ()
-		{
-				if (!caller_ends_display) {
-						if (showTime > 0)
-								showTime--;
-						if (showTime == 0) {
-								EndDisplay ();
-						}
-				}
-				
-		}
-
-		public bool WordImageIsOnDisplay ()
-		{
-				return img.enabled;
-
-
-		}
-
-
-		
-
-
 
 
 }
