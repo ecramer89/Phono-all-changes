81e577a9fab922c3e55a319b24e54fb849d927c0
Emily Cramer <emily.cramer@adaptechsolutions.ca>
2017-10-30 17:42:43 -0700

    hooked up back button

--- Assets/PhonoBlocks/scripts/Main Menu/BackButton.cs
+++ Assets/PhonoBlocks/scripts/Main Menu/BackButton.cs
@@ -17,6 +17,7 @@ public class BackButton : MonoBehaviour {
 
 
 	void GoBack(){
+		Debug.Log("called go back");
 		Stack<Action> mainMenuNavStack = Transaction.Instance.State.MainMenuNavigationStack;
 		if(mainMenuNavStack.Count>0) mainMenuNavStack.Pop()();
 	}
