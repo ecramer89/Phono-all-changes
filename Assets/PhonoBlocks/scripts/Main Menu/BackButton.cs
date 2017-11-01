09f6fc9aa0f7ccc248a4079280a7a1da6e78f61f
Emily Cramer <emily.cramer@adaptechsolutions.ca>
2017-11-01 07:23:48 -0700

    back button only visible if there is a state in nav stackyou can navigate back from. adjusted some of the images

--- Assets/PhonoBlocks/scripts/Main Menu/BackButton.cs
+++ Assets/PhonoBlocks/scripts/Main Menu/BackButton.cs
@@ -6,19 +6,28 @@ using System;
 
 [RequireComponent(typeof(UIButtonMessage))]
 [RequireComponent(typeof(UIImageButton))]
-public class BackButton : MonoBehaviour {
+public class BackButton : PhonoBlocksSubscriber {
 
 	void Start(){
 		UIButtonMessage messenger = GetComponent<UIButtonMessage> ();
 		messenger.target = gameObject;
 		messenger.functionName = "GoBack";
 		messenger.trigger = UIButtonMessage.Trigger.OnClick;
+		gameObject.SetActive(false);
+
+	}
+
+	public override void SubscribeToAll(PhonoBlocksScene forScene){
+		if(forScene != PhonoBlocksScene.MainMenu) return;
+		Transaction.Instance.MainMenuNavigationStateChanged.Subscribe(this, (ParameterlessEvent obj) => {
+			gameObject.SetActive(true);
+		});
 	}
 
 
 	void GoBack(){
-		Debug.Log("called go back");
 		Stack<ParameterlessEvent> mainMenuNavStack = Transaction.Instance.State.MainMenuNavigationStack;
 		if(mainMenuNavStack.Count>0) mainMenuNavStack.Pop().Fire();
+		gameObject.SetActive(mainMenuNavStack.Count>0);
 	}
 }
